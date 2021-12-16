59f80be0550a3ab1bf45e4f79264bc6457a60df4
msantiago9 <marjonsantiago49@gmail.com>
2021-12-16 11:21:55 -0500

    init Heroku

---
+++ static/themeToggler.js
@@ -0,0 +1,11 @@
+let light = document.querySelector("link[href='/static/light.css']") !== null;
+const toggleTheme = () => {
+  if (light) {
+    document.querySelector("link[href='/static/light.css']").href =
+      "/static/dark.css";
+  } else {
+    document.querySelector("link[href='/static/dark.css']").href =
+      "/static/light.css";
+  }
+  light = !light;
+};
